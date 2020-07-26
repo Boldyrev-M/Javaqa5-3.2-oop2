3ffe5c2e46f26fa9ab8f059b0da18024b6677e47
Maxim Boldyrev <maxikb@mail.ru>
2020-07-26 19:53:34 +0300

    info from vk api docs added

--- src/main/java/ru/netology/domain/CommentsInfo.java
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -4,9 +4,9 @@ import java.util.Date;
 
 public class CommentsInfo {
     private int commentId; //
-    private int postId; // к какому посту относится
-    private int replyToId; // ответ на комментарий
-    private Date commentDate; // дата публикации
-    private String commentText; // текст комментария
     private int userId; // автор коммента
+    private int commentDate; // дата создания комментария в формате Unixtime
+    private String commentText; // текст комментария
+    private int replyToUser; // идентификатор пользователя или сообщества, в ответ которому оставлен текущий комментарий (если применимо)
+    private int replyToComment; // ответ на комментарий
 }
--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -4,16 +4,36 @@ import java.util.Date;
 
 public class Post {
     private int id; // id поста
-    private int userId; // автор поста
-    private String postHeader; // заголовок поста
+    private int fromId; // автор поста
+    private int ownerId; // владелец стены, на которой размещена запись
+    private int createdBy; // идентификатор администратора, который опубликовал запись
+
+    //private String postHeader; // заголовок поста
+    private int date; // время публикации записи в формате unixtime
     private String postText; // текст поста
-    private Date postDate; // дата публикации
+    private int replyOwnerId; // идентификатор владельца записи, в ответ на которую была оставлена текущая
+    private  int replyPostId; // идентификатор записи, в ответ на которую была оставлена текущая
+    private int friendsOnly; // 1, если запись была создана с опцией «Только для друзей»
+
     private int theme; // тематика поста
     private int visibleTo; // видимость
-    private int postType; //    тип поста: документ, карта, граффити, товар, опрос
+    private String postType; //    тип записи, может принимать следующие значения: post, copy, reply, postpone, suggest
     private int[] likeUser; // лайки - id пользователей
     private CommentsInfo[] commentsInfo; // ссылки на комменты
-    private boolean commentsOff; // отключить комментарии?
+    //private boolean commentsOff; // отключить комментарии?
+
+    private int viewCount; // число просмотров записи
+    private String copyright; // источник материала
+    private int signerId; // идентификатор автора, если запись была опубликована от имени сообщества и подписана пользователем
+    private boolean canPin; //информация о том, может ли текущий пользователь закрепить запись (1 — может, 0 — не может)
+    private boolean canDelete; //информация о том, может ли текущий пользователь удалить запись (1 — может, 0 — не может)
+    private boolean canEdit; // информация о том, может ли текущий пользователь редактировать запись (1 — может, 0 — не может)
+    private boolean isPinned; // информация о том, что запись закреплена
+    private int marked_as_ads; // информация о том, содержит ли запись отметку "реклама" (1 — да, 0 — нет).
+    private boolean is_favorite; // true, если объект добавлен в закладки у текущего пользователя
+    private int postponed_id; //идентификатор отложенной записи. Это поле возвращается тогда, когда запись стояла на таймере.
 
 
+    public Post() {
+    }
 }
